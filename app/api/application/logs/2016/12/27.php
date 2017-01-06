d87a42d6e7c12611396ab72d9304cf2a897eee30
kyselov dmytro <dkiselov.ua@gmail.com>
2017-01-06 14:02:43 +0200

    app_core

---
+++ app/api/application/logs/2016/12/27.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2016-12-27 15:53:16 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL application/server was not found on this server. ~ SYSPATH\classes\kohana\request\client\internal.php [ 94 ]
---
+++ app/api/application/logs/2017/01/03.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') OR die('No direct script access.'); ?>
+
+2017-01-03 18:24:42 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2017-01-03 18:30:16 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2017-01-03 18:30:49 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2017-01-03 18:37:45 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2017-01-03 18:39:02 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2017-01-03 18:39:03 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
+2017-01-03 18:39:04 --- ERROR: HTTP_Exception_404 [ 404 ]: The requested URL dv_app/app/server was not found on this server. ~ SYSPATH/classes/kohana/request/client/internal.php [ 94 ]
